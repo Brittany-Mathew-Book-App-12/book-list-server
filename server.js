46eec2541da60da5efcb40410782aeada1103f5e
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-04-21 13:41:12 -0700

    working

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
-// const DATABASE_URL = 'postgres://localhost:5432/books_app';
-const DATABASE_URL = 'postgres://localhost/books_app';
+// const DATABASE_URL = process.env.DATABASE_URL;
 
 
 // Application Setup
@@ -15,11 +14,11 @@ const PORT = process.env.PORT || 3000; //delete on saturday
 const CLIENT_URL = process.env.CLIENT_URL;
 
 // Database Setup
-const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/books_app');
+const client = new pg.Client(process.env.DATABASE_URL); //|| 'postgres://localhost/books_app');
 client.connect();
 client.on('error', err => console.error(err));
 
-const apiUrl = '/api/vi/books';
+const apiUrl = '/api/v1/books';
 
 // Application Middleware
 app.use(cors());
@@ -28,24 +27,25 @@ app.use(express.json());
 
 
 // API Endpoints
-app.get(apiUrl, (res, req) => {
+app.get(apiUrl, (req, res) => {
   client.query(`
   SELECT book_id, title, author, image_url, isbn FROM books;`)
     .then(results => res.send(results.rows)
       .catch(console.error));
 });
 
-app.get(apiUrl + '/id', (res, req) => {
+app.get(apiUrl + '/:id', (req, res) => {
   client.query(`
   
   SELECT * FROM books WHERE book_id=$1
 
 `, [req.params.id])
-    .then(results => res.setEncoding(results.rows))
-    .catch(err => res.sendStatus(500));
+    .then(results => res.send(results.rows))
+    // .catch(err => res.sendStatus(500));
+    .catch(console.error);
 });
 
-app.post(apiUrl, (res, req) => {
+app.post(apiUrl, (req, res) => {
   client.query(`
   
   INSERT INTO books
@@ -61,5 +61,5 @@ app.post(apiUrl, (res, req) => {
     .catch(console.error);
 });
 
-app.get('*', (req, res) => res.redirect(CLIENT_URL));
+// app.get('*', (req, res) => res.redirect(CLIENT_URL));
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
